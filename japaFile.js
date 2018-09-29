4451fd6427c7fbd95c265058263ef4a5514a18b8
LookinGit <alook.in@yandex.ru>
2018-09-29 16:24:06 +0500

    chore(package): update japa tests

---
+++ japaFile.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+ * adonis-lucid-filter
+ *
+ * (c) Lookin Anton <lookin@lookinlab.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const { configure } = require('japa')
+
+configure({
+  files: ['test/*.spec.js']
+})
