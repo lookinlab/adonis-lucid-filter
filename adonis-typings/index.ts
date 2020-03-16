7c42c945d5109f799ec9d9a98f5f1441b081a8ca
LookinGit <alook.in@yandex.ru>
2020-03-16 16:15:27 +0300

    chore(typings): clear exports

--- adonis-typings/index.ts
+++ adonis-typings/index.ts
@@ -1,7 +1,3 @@
 declare module '@ioc:Adonis/Addons/LucidFilter' {
-  export interface LucidFilterContract {
-    $input: object
-    setup? (query: any): void
-  }
-  export const filterable: (Filter: LucidFilterContract) => (constructor: Function) => void
+  export const LucidFilter
 }
