f78427a1b0a3988a0f3281be1ae94aa4c168b72f
LookinGit <alook.in@yandex.ru>
2024-02-14 20:18:23 +0500

    chore: add breaklines

--- stubs/main.ts
+++ stubs/main.ts
@@ -6,5 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 import { getDirname } from '@poppinss/utils'
+
 export const stubsRoot = getDirname(import.meta.url)
